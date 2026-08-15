5c169de1c76a9b024b6966a9cde4e2f81a9e595e
Palak Singh <palak27055@gmail.com>
2026-08-15 12:40:34 +0530

    maximum wealth

--- Searching/MaxWealth.java
+++ Searching/MaxWealth.java
@@ -1,10 +1,27 @@
 package Searching;
-
+import java.util.*;
 public class MaxWealth {
     public static void main(String[] args) {
+        int[][] accounts = {
+            {3, 4, 5},
+            {5, 6, 6},
+            {3, 5, 2}
+        };
+        System.out.println(maximumwealth(accounts));
         
     }
     static int maximumwealth(int[][] accounts){
-        
+        int ans = Integer.MIN_VALUE;
+        for( int[] ints : accounts){
+            int sum =0;
+            for(int anInt : ints){
+                sum += anInt;
+            }
+        if(sum>ans){
+            ans = sum;
+        }
+    }
+        return ans;
+    
     }
 }
